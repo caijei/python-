9ee8f8605344faa9be21b4f6d76420724730ca4e
caijei <129049073+caijei@users.noreply.github.com>
2024-07-05 10:48:19 +0800

    Add files via upload

--- 课设.py
+++ 课设.py
@@ -5,16 +5,28 @@ from 爬虫 import UA_pool
 from 爬虫.temp import get_header, province_header, mp
 
 
-def save_data(s, data):
-    with open(f'D:\\py课设\\高校分数线{s}.csv', encoding='UTF-8', mode='a+', newline='') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerow(data)
-
-
 headers = {
     'User-Agent': UA_pool.get_ua()
 }
 
+province_id = [{"name": 11, "value": "北京"}, {"name": 12, "value": "天津"}, {"name": 13, "value": "河北"},
+               {"name": 14, "value": "山西"}, {"name": 15, "value": "内蒙古"}, {"name": 21, "value": "辽宁"},
+               {"name": 22, "value": "吉林"}, {"name": 23, "value": "黑龙江"}, {"name": 31, "value": "上海"},
+               {"name": 32, "value": "江苏"}, {"name": 33, "value": "浙江"}, {"name": 34, "value": "安徽"},
+               {"name": 35, "value": "福建"}, {"name": 36, "value": "江西"}, {"name": 37, "value": "山东"},
+               {"name": 41, "value": "河南"}, {"name": 42, "value": "湖北"}, {"name": 43, "value": "湖南"},
+               {"name": 44, "value": "广东"}, {"name": 45, "value": "广西"}, {"name": 46, "value": "海南"},
+               {"name": 50, "value": "重庆"}, {"name": 51, "value": "四川"}, {"name": 52, "value": "贵州"},
+               {"name": 53, "value": "云南"}, {"name": 54, "value": "西藏"}, {"name": 61, "value": "陕西"},
+               {"name": 62, "value": "甘肃"}, {"name": 63, "value": "青海"}, {"name": 64, "value": "宁夏"},
+               {"name": 65, "value": "新疆"}]
+#会生成一个字典，表示每个省的表头是什么
+get_header()
+
+def save_data(s, data):
+    with open(f'D:\\py课设\\高校分数线{s}.csv', encoding='UTF-8', mode='a+', newline='') as f:
+        f_csv = csv.writer(f)#创建对象
+        f_csv.writerow(data)#写入数据
 
 def get_url(url):
     try:
@@ -27,9 +39,7 @@ def get_url(url):
     html_str = response.text
     return html_str
 
-
 def func(m):
-    s = ' '
     #sorted_dict = {key: my_dict[key] for key in sorted(my_dict)}
     m['type'] = {key: m['type'][key] for key in sorted(m['type'])}
     fen_list = []
@@ -50,30 +60,18 @@ def check(x):
     else:
         return '否'
 
-
+#获取学校的id以名字
 url = 'https://static-data.gaokao.cn/www/2.0/school/name.json'
 html = requests.get(url).text
 unicodestr = json.loads(html)  # 将string转化为dict
 dat = unicodestr["data"]
 
-province_id = [{"name": 11, "value": "北京"}, {"name": 12, "value": "天津"}, {"name": 13, "value": "河北"},
-               {"name": 14, "value": "山西"}, {"name": 15, "value": "内蒙古"}, {"name": 21, "value": "辽宁"},
-               {"name": 22, "value": "吉林"}, {"name": 23, "value": "黑龙江"}, {"name": 31, "value": "上海"},
-               {"name": 32, "value": "江苏"}, {"name": 33, "value": "浙江"}, {"name": 34, "value": "安徽"},
-               {"name": 35, "value": "福建"}, {"name": 36, "value": "江西"}, {"name": 37, "value": "山东"},
-               {"name": 41, "value": "河南"}, {"name": 42, "value": "湖北"}, {"name": 43, "value": "湖南"},
-               {"name": 44, "value": "广东"}, {"name": 45, "value": "广西"}, {"name": 46, "value": "海南"},
-               {"name": 50, "value": "重庆"}, {"name": 51, "value": "四川"}, {"name": 52, "value": "贵州"},
-               {"name": 53, "value": "云南"}, {"name": 54, "value": "西藏"}, {"name": 61, "value": "陕西"},
-               {"name": 62, "value": "甘肃"}, {"name": 63, "value": "青海"}, {"name": 64, "value": "宁夏"},
-               {"name": 65, "value": "新疆"}]
-
-get_header()
-
+#写入每个省的表头
 for l in province_id:
     with open('D:\\py课设\\高校分数线' + l["value"] + '.csv', encoding='utf-8-sig', mode='w', newline='') as f:
         f_csv = csv.writer(f)
         f_csv.writerow(province_header[f'{l['name']}'])
+
 for i in dat:
     schoolid = i['school_id']
     schoolname = i['name']
@@ -93,7 +91,7 @@ for i in dat:
 
         f211 = check(dat1['f211'])
 
-        type_name = dat1["type_name"]  #
+        type_name = dat1["type_name"]  #学校属性
         school_nature_name = dat1["school_nature_name"]  #学校是公办还是民办
         province_name = dat1["province_name"]  #在那个省学校
         city_name = dat1["city_name"]  #学校在那个市
